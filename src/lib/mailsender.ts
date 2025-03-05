9cfa1058bdf2b8f78dccbf4604aa886220b4cb72
DwaftPainter <2301140014@ms.hanu.edu.vn>
2025-03-05 20:13:44 +0700

    fix(production): mail sender delay

--- src/lib/mailsender.ts
+++ src/lib/mailsender.ts
@@ -11,22 +11,19 @@ const options: SMTPConnection.Options = {
     }
 }
 
-
 export const mailSender = async (receiver: any, subject?: any, text?: any, html?: any) => {
     const transporter = nodemailer.createTransport(options)
-    console.log("🚀 ~ tracker 3 ~ mailSender ~ transporter:", transporter)
-    try {
-        const info = await transporter.sendMail({
+    console.log('🚀 ~ tracker 3 ~ mailSender ~ transporter:', transporter)
+    const sendMessage = async () => {
+        await transporter.sendMail({
             from: process.env.SMTP_USER, // sender address
             to: receiver, // list of receivers
             subject: subject, // Subject line
             text: text, // plain text body
             html: html // html body
         })
-
-        console.log('🚀 ~ mailSender ~ info:', info)
-    } catch (error) {
-        console.log(error)
     }
 
+    await sendMessage()
+    console.log("Sent")
 }
